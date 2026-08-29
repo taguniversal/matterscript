02d6c95d302706dd83d77c6023195848f5fcad13
Enrique Flores <taguniversalsoftware@gmail.com>
2026-08-29 12:17:06 -0500

    mdbook Generate Block

--- docs/mdbook/book/searcher-09f2665d.js
+++ docs/mdbook/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-eb609030.js',
+                path_to_root + 'searchindex-06e12a54.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
