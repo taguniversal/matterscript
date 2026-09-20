946ac8a2590ba6bca0f737f39bf618912275d81c
Enrique Flores <taguniversalsoftware@gmail.com>
2026-09-20 15:39:23 -0500

    Add mdbook Edges and Loops

--- docs/mdbook/book/html/searcher-09f2665d.js
+++ docs/mdbook/book/html/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e88e15cb.js',
+                path_to_root + 'searchindex-b0284014.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
